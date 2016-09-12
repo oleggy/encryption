7d3a8bfe7a241eb2fb585494ec51f89b6a0d602e
oleggy <oleggy@ya.ru>
2016-09-12 21:13:30 +0300

    SHA-2 added

--- encryption.rb
+++ encryption.rb
@@ -7,8 +7,8 @@ user_input = STDIN.gets.chomp
 
 encrypt_method = 0
 
-while !([1,2].include?(encrypt_method)) do
-  puts "\nКаким методом шифруем?\n1. MD5\n2. SHA-1\n"
+while !([1,2,3].include?(encrypt_method)) do
+  puts "\nКаким методом шифруем?\n1. MD5\n2. SHA-1\n3. SHA-2\n"
   encrypt_method = STDIN.gets.to_i
 end
 
@@ -17,4 +17,6 @@ case encrypt_method
     puts "\nВот, что получилось:\n " + Digest::MD5.hexdigest(user_input)
   when 2
     puts "Вот, что получилось:\n" + Digest::SHA1.hexdigest(user_input)
+  when 3
+    puts "Вот, что получилось:\n" + Digest::SHA2.hexdigest(user_input)
 end
